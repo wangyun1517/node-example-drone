4a82f875d23ac40c2e608a6063f62e0856f28bc6
David Gonzalez <nodearth@gmail.com>
2016-12-09 17:07:06 +0000

    would it work?

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello David bbb!')
+  res.send('Hello David ccc!')
 })
 
 app.listen(3000, function () {
