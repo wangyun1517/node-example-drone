371921cd9a22db73516660a279f4c70c2dfd4cfb
David Gonzalez <nodearth@gmail.com>
2016-12-12 10:23:12 +0000

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello David ccc!')
+  res.send('Hello David!')
 })
 
 app.listen(3000, function () {
